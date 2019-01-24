76a5f0d589238991c65201398d1c052b9536c70c
Ahmed Abdallah <ahmedabdallah@Ahmeds-MacBook-Pro.local>
2019-01-24 19:49:21 +0300

    Create Xcode project

---
+++ MVPExampleTests/MVPExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  MVPExampleTests.swift
+//  MVPExampleTests
+//
+//  Created by Ahmed Abdallah on 24.01.2019.
+//  Copyright © 2019 4A5. All rights reserved.
+//
+
+import XCTest
+@testable import MVPExample
+
+class MVPExampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
